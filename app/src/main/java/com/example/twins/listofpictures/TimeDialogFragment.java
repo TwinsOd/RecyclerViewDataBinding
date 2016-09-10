f4bf6c8f118162eaef0f5f2f7580f33295ba75ee
Zakharov Vladymyr <ZaharovVova@yandex.ru>
2016-09-10 12:57:08 +0200

    add imageView to dialogFragment

--- app/src/main/java/com/example/twins/listofpictures/TimeDialogFragment.java
+++ app/src/main/java/com/example/twins/listofpictures/TimeDialogFragment.java
@@ -3,12 +3,17 @@ package com.example.twins.listofpictures;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 
 public class TimeDialogFragment extends DialogFragment {
     private static final String ARG_HOUR = "hour";
+    private static final String URL = "https://static-s.aa-cdn.net/img/ios/928952797/fd4ca6f3830886a6d91716d625c2a9bd?v=1";
     private String hour;
 
 
@@ -29,12 +34,16 @@ public class TimeDialogFragment extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Context context = getActivity();
+        ImageView imageView = new ImageView(context);
+
         if (getArguments() != null) {
             hour = getArguments().getString(ARG_HOUR);
         }
 
-        return new AlertDialog.Builder(getActivity())
+        Dialog dialog = new AlertDialog.Builder(getActivity())
                 .setIcon(R.drawable.ic_notifications_active_24dp)
+                .setView(imageView)
                 .setTitle("now it is  " + hour)
                 .setPositiveButton(R.string.alert_dialog_ok,
                         new DialogInterface.OnClickListener() {
@@ -44,5 +53,12 @@ public class TimeDialogFragment extends DialogFragment {
                         }
                 )
                 .create();
+
+        Picasso.with(context)
+                .load(URL)
+                .placeholder(R.drawable.ic_crop_original_100dp)
+                .into(imageView);
+
+        return dialog;
     }
 }
